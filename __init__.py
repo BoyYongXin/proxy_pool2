6b2e9ef47adeb5bb6bcb7761830f2814c37bc794
yangyongxin <yangyongxin@yidian-inc.com>
2021-03-12 11:41:41 +0800

    update: check_proxy.py and db and settings.py files

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 # nohup python3 cmd.py --name init --api https://eproxy.ppio.cloud/proxy_list?num=20&zone=1 --url http://www.baidu.com > /dev/null 2>&1 &
 # nohup python3 cmd.py --name init --api https://eproxy.ppio.cloud/proxy_list?num=20&zone=2 --url http://www.baidu.com > /dev/null 2>&1 &
 # nohup python3 cmd.py --name init --api https://eproxy.ppio.cloud/proxy_list?num=20&zone=3 --url http://www.baidu.com > /dev/null 2>&1 &
-# nohup python3 cmd.py --name init --api https://eproxy.ppio.cloud/proxy_list?num=20&zone=4 --url http://www.baidu.com > /dev/null 2>&1 &
+# nohup python3 cmd.py --name init --api https://eproxy.ppio.cloud/proxy_list?num=20&zone=4 --url http://www.baidu.com
 
 
 #python3 cmd.py --name init --api https://eproxy.ppio.cloud/proxy_list  --url http://www.baidu.com
